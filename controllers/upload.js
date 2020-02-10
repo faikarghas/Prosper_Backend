37f2945c8aadaa0e3c747c53352a0f9d98d7fd1b
faikar <ghassanfaikar@yahoo.com>
2020-02-10 13:10:07 +0700

    initial commit

--- controllers/upload.js
+++ controllers/upload.js
@@ -3,8 +3,30 @@ var fs  = require('fs')
 module.exports = {
     uploadData : (req, res) => {
 
-        if(req.body.submit === false){
 
+        let dir = "./document"
+        console.log( req.files.file.length);
+        if (req.files.file.length !== undefined) {
+            if (!fs.existsSync()){
+                // Creates /tmp/a/apple, regardless of whether `/tmp` and /tmp/a exist.
+                fs.mkdir(dir, { recursive: true }, (err) => {
+                    for (let i = 0; i < req.files.file.length; i++) {
+                        let imageFile = req.files.file[i];
+                        let filename = imageFile.name
+                        let fileNameWithoutSpace = filename.replace(/\s/g, '');
+                        imageFile.mv(`${__dirname}/../document/${fileNameWithoutSpace}`, function (err) {
+                            if (err) {
+                                return res.status(500).send(err);
+                            }
+                        });
+                    }
+                    // if (err) throw err;
+                });
+                // fs.mkdirSync(dir);
+                return res.status(200).send({success:true});
+            }else {
+                console.log("Directory already exist");
+            }
         } else {
             let imageFile = req.files.file;
             let filename = imageFile.name
@@ -12,33 +34,11 @@ module.exports = {
             imageFile.mv(`${__dirname}/../document/${fileNameWithoutSpace}`, function (err) {
                 if (err) {
                     return res.status(500).send(err);
+                } else{
+                    return res.status(200).send({success:true});
                 }
             });
         }
-
-        if (!fs.existsSync()){
-
-            // Creates /tmp/a/apple, regardless of whether `/tmp` and /tmp/a exist.
-            // fs.mkdir(dir, { recursive: true }, (err) => {
-            //     let imageFile = req.files.file;
-            //     let filename = imageFile.name
-            //     let fileNameWithoutSpace = filename.replace(/\s/g, '');
-            //     console.log(fileNameWithoutSpace);
-
-            //     imageFile.mv(`${__dirname}/../${dir}/${fileNameWithoutSpace}`, function (err) {
-            //         if (err) {
-            //             return res.status(500).send(err);
-            //         }
-            //     });
-
-            //     console.log(err);
-            //     // if (err) throw err;
-            // });
-            // fs.mkdirSync(dir);
-        }else {
-            console.log("Directory already exist");
-        }
-        res.json({test:'test'})
     },
     deleteImage : (req, res) => {
         fs.unlink('coba.png', function (err) {
